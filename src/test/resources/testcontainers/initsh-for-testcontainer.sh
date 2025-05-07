ee4c3c7eafa3a77e3281a679d38203c28d323cb3
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-05-07 16:12:13 +0200

    [PN-14790] fix: updated initsh-for-testcontainer.sh

--- src/test/resources/testcontainers/initsh-for-testcontainer.sh
+++ src/test/resources/testcontainers/initsh-for-testcontainer.sh
@@ -1,7 +1,5 @@
 echo "### CREATE DELIVERY-PUSH COMPONENT ###"
-## TODO: Aggiornare commitId con quello del branch
-bash <(curl -s https://raw.githubusercontent.com/pagopa/pn-timeline-service/e1646bf00880ab7f1fdcbc3ba98e9213d4abe41a/src/test/resources/testcontainers/init.sh)
-
+bash <(curl -s https://raw.githubusercontent.com/pagopa/pn-timeline-service/244b17615bc2fee61b648e5a32b478f00bd80e2f/src/test/resources/testcontainers/init.sh)
 done
 
 
