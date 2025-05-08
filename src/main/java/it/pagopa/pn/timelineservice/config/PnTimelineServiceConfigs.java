30282f85450fc194b861289ce1fb6414ca7f2bfa
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-05-08 10:53:49 +0200

    [PN-14790] fix: post merge removed unused configs

--- src/main/java/it/pagopa/pn/timelineservice/config/PnTimelineServiceConfigs.java
+++ src/main/java/it/pagopa/pn/timelineservice/config/PnTimelineServiceConfigs.java
@@ -1,17 +1,14 @@
 package it.pagopa.pn.timelineservice.config;
 
 import it.pagopa.pn.commons.conf.SharedAutoConfiguration;
-import it.pagopa.pn.timelineservice.dto.address.PhysicalAddressInt;
+import jakarta.annotation.PostConstruct;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.util.unit.DataSize;
 
-import javax.annotation.PostConstruct;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.List;
 
 @Configuration
 @ConfigurationProperties( prefix = "pn.timeline-service")
@@ -19,116 +16,15 @@ import java.util.List;
 @Import({SharedAutoConfiguration.class})
 public class PnTimelineServiceConfigs {
 
-    private String templatesEngineBaseUrl;
-
     private String deliveryBaseUrl;
 
-    private String paperChannelBaseUrl;
-
-    private String externalChannelBaseUrl;
-
-    private String externalchannelCxId;
-
-    private String externalchannelSenderPec;
-
-    private String externalchannelSenderEmail;
-
-    private String externalchannelSenderSms;
-
     private String dataVaultBaseUrl;
-
-    private String safeStorageBaseUrl;
-
-    private String safeStorageCxId;
-
-    private String safeStorageCxIdUpdatemetadata;
-
-    private String userAttributesBaseUrl;
-
-    private String externalRegistryBaseUrl;
-
-    private String mandateBaseUrl;
-
-    private Topics topics;
-
-    private Instant actionPoolEpoch;
-
-    private Duration actionPoolBeforeDelay;
-
-    private Webhook webhook;
-
-    private Webapp webapp;
 
     private TimelineDao timelineDao;
 
     private TimelinecounterDao timelinecounterDao;
 
-    private FailedNotificationDao failedNotificationDao;
-
-    private ActionDao actionDao;
-
-    private WebhookDao webhookDao;
-
-    private FutureActionDao futureActionDao;
-
     private LastPollForFutureActionDao lastPollForFutureActionDao;
-
-    private DocumentCreationRequestDao documentCreationRequestDao;
-
-    private ExternalChannel externalChannel;
-
-    private PaperChannel paperChannel;
-
-    private Integer retentionAttachmentDaysAfterRefinement;
-
-    private String nationalRegistriesBaseUrl;
-
-    private String addressManagerBaseUrl;
-
-    private String f24BaseUrl;
-
-    private String emdIntegrationBaseUrl;
-
-    private String addressManagerApiKey;
-
-    private Duration[] validationRetryIntervals;
-
-    private DataSize checkPdfSize;
-
-    private boolean checkPdfValidEnabled;
-
-    private String actionTtlDays;
-
-    private boolean checkCfEnabled;
-
-    private String f24CxId;
-
-    private int pagoPaNotificationBaseCost;
-
-    private int pagoPaNotificationFee;
-
-    private int pagoPaNotificationVat;
-
-    private List<String> paperSendMode;
-
-    private List<String> raddExperimentationStoresName;
-
-    private List<String> pnSendMode;
-
-    private String activationDeceasedWorkflowDate;
-
-    //quickWorkAroundForPN-9116
-    private boolean sendMoreThan20GramsDefaultValue;
-
-    private List<String> listCategoriesPa;
-
-//    private ErrorCorrectionLevel errorCorrectionLevelQrCode;
-
-    private String performanceImprovementStartDate;
-
-    private String performanceImprovementEndDate;
-
-    private boolean additionalLangsEnabled;
 
     private Instant featureUnreachableRefinementPostAARStartDate;
 
@@ -142,114 +38,10 @@ public class PnTimelineServiceConfigs {
 
     private Duration timelineLockDuration;
 
-    private String AAROnlyPECForRADDAndPF;
-
-    private String templateURLforPEC;
-
-
-    @Data
-    public static class Topics {
-
-        private String newNotifications;
-
-        private String scheduledActions;
-
-        private String executedActions;
-
-        private String toExternalChannelPec;
-
-        private String toExternalChannelEmail;
-
-        private String toExternalChannelPaper;
-
-        private String fromExternalChannel;
-
-        private String safeStorageEvents;
-
-        private String nationalRegistriesEvents;
-
-        private String addressManagerEvents;
-
-        private String f24Events;
-
-        private String deliveryValidationEvents;
-    }
-
-    @Data
-    public static class Webhook {
-        private Long scheduleInterval;
-        private Integer maxLength;
-        private Integer purgeDeletionWaittime;
-        private Integer readBufferDelay;
-        private Integer maxStreams;
-        //Delta utilizzato per il counter di uno stream di sostituzione
-        private Integer deltaCounter;
-        private Duration ttl;
-        private Duration disableTtl;
-        private String firstVersion;
-        private String currentVersion;
-    }
-
-    @Data
-    public static class ExternalChannel {
-
-        private List<String> digitalCodesProgress;
-        private List<String> digitalCodesSuccess;
-        private List<String> digitalCodesFail;
-        private List<String> digitalCodesRetryable;
-
-        private List<String> digitalCodesFatallog;
-
-        private int digitalRetryCount;
-        private Duration digitalRetryDelay;
-        private Duration digitalSendNoresponseTimeout;
-
-    }
-
-    @Data
-    public static class SenderAddress {
-        private String fullname;
-        private String address;
-        private String zipcode;
-        private String city;
-        private String pr;
-        private String country;
-    }
-
-   @Data
-   public static class Webapp {
-        private String directAccessUrlTemplatePhysical;
-        private String directAccessUrlTemplateLegal;
-        private String faqUrlTemplateSuffix;
-        private String faqSendHash;
-        private String quickAccessUrlAarDetailSuffix;
-        private String landingUrl;
-        private String raddPhoneNumber;
-        private String aarSenderLogoUrlTemplate;
-   }
-
-    @Data
-    public static class PaperChannel {
-
-        private SenderAddress senderAddress;
-
-        public PhysicalAddressInt getSenderPhysicalAddress(){
-            return PhysicalAddressInt.builder()
-                    .fullname(senderAddress.getFullname())
-                    .address(senderAddress.getAddress())
-                    .zip(senderAddress.getZipcode())
-                    .province(senderAddress.getPr())
-                    .municipality(senderAddress.getCity())
-                    .foreignState(senderAddress.getCountry())
-                    .build();
-        }
-    }
-
     @Data
     public static class TimelineDao {
         private String tableName;
     }
-
 
     @Data
     public static class TimelinecounterDao {
@@ -257,38 +49,10 @@ public class PnTimelineServiceConfigs {
     }
 
     @Data
-    public static class FailedNotificationDao {
-        private String tableName;
-    }
-
-    @Data
-    public static class ActionDao {
-        private String tableName;
-    }
-
-    @Data
-    public static class FutureActionDao {
-        private String tableName;
-    }
-
-    @Data
-    public static class WebhookDao {
-        private String streamsTableName;
-        private String eventsTableName;
-    }
-
-    @Data
     public static class LastPollForFutureActionDao {
         private String tableName;
         private String lockTableName;
     }
-
-    @Data
-    public static class DocumentCreationRequestDao {
-        private String tableName;
-    }
-
-    private boolean safeStorageFileNotFoundRetry;
 
     @PostConstruct
     public void init() {
