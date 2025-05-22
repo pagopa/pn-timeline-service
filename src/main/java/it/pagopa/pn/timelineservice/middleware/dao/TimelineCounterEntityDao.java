abca7feb29227d8eaf598906953896d31f3c395b
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-05-22 12:41:18 +0200

    [PN-14902] fix: removed useless execution in pom.xml and removed unused PnDataVaultClient

--- src/main/java/it/pagopa/pn/timelineservice/middleware/dao/TimelineCounterEntityDao.java
+++ src/main/java/it/pagopa/pn/timelineservice/middleware/dao/TimelineCounterEntityDao.java
@@ -4,9 +4,5 @@ import it.pagopa.pn.timelineservice.middleware.dao.dynamo.entity.TimelineCounter
 import reactor.core.publisher.Mono;
 
 public interface TimelineCounterEntityDao {
-
-
-    String IMPLEMENTATION_TYPE_PROPERTY_NAME = "pn.middleware.impl.timeline-counter-dao";
-
     Mono<TimelineCounterEntity> getCounter(String timelineElementId);
 }
