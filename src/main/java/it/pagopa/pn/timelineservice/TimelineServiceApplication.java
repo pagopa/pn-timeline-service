605760c3997f2af040c16f1a1598866aa27573db
ECARADM5R <matteo.caradonna@emeal.nttdata.com>
2025-05-28 17:27:35 +0200

    [PN-14846] - Spring Analyzer init

--- src/main/java/it/pagopa/pn/timelineservice/TimelineServiceApplication.java
+++ src/main/java/it/pagopa/pn/timelineservice/TimelineServiceApplication.java
@@ -1,11 +1,14 @@
 package it.pagopa.pn.timelineservice;
 
+import it.pagopa.pn.commons.configs.listeners.TaskIdApplicationListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TimelineServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TimelineServiceApplication.class, args);
+        SpringApplication app = new SpringApplication(TimelineServiceApplication.class);
+        app.addListeners(new TaskIdApplicationListener());
+        app.run(args);
     }
 }
