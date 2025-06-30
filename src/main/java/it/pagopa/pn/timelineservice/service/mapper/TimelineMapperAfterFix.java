a211087a87a231627ce44f1a24bca5d4405cfbe3
empassaro <emanuele.passaro@emeal.nttdata.com>
2025-06-30 14:58:37 +0200

    fix: removed sonar issues

--- src/main/java/it/pagopa/pn/timelineservice/service/mapper/TimelineMapperAfterFix.java
+++ src/main/java/it/pagopa/pn/timelineservice/service/mapper/TimelineMapperAfterFix.java
@@ -20,39 +20,9 @@ public class TimelineMapperAfterFix extends TimelineMapper {
 
             //Nello switch case invece vengono effettuati ulteriori remapping dei timestamp, questi non dipendono dal singolo elemento, ma necessitano di tutta la timeline
             switch (result.getCategory()) {
-                case SCHEDULE_REFINEMENT -> {
-                    //Se per lo stesso destinatario è presente un elemento di ANALOG_FAILURE_WORKFLOW, viene presa come riferimento la data di questo evento.
-                    //In caso contrario, viene presa come riferimento la data dell’evento più recente tra PREPARE_ANALOG_DOMICILE_FAILURE e SEND_ANALOG_FEEDBACK
-                    Instant analogFailureWorkflowDate = getAnalogFailureWorkflowDate((RecipientRelatedTimelineElementDetails) result.getDetails(), timelineElementInternalSet, result.getIun());
-                    if (analogFailureWorkflowDate != null) {
-                        result.setTimestamp(analogFailureWorkflowDate);
-                    } else {
-                        Instant endAnalogWorkflowBusinessDate = computeEndAnalogWorkflowBusinessData((RecipientRelatedTimelineElementDetails) result.getDetails(), timelineElementInternalSet, result.getIun());
-                        if (endAnalogWorkflowBusinessDate != null) {
-                            log.debug("MAP TIMESTAMP: elem category {}, elem previous timestamp {}, elem new timestamp {} ", result.getCategory(), result.getTimestamp(), endAnalogWorkflowBusinessDate);
-                            result.setTimestamp(endAnalogWorkflowBusinessDate);
-                        }
-                    }
-                }
-                case ANALOG_SUCCESS_WORKFLOW, ANALOG_WORKFLOW_RECIPIENT_DECEASED -> {
-                    //Viene presa come riferimento la data dell’evento più recente tra PREPARE_ANALOG_DOMICILE_FAILURE e SEND_ANALOG_FEEDBACK
-                    Instant endAnalogWorkflowBusinessDate = computeEndAnalogWorkflowBusinessData((RecipientRelatedTimelineElementDetails) result.getDetails(), timelineElementInternalSet, result.getIun());
-                    if (endAnalogWorkflowBusinessDate != null) {
-                        log.debug("MAP TIMESTAMP: elem category {}, elem previous timestamp {}, elem new timestamp {} ", result.getCategory(), result.getTimestamp(), endAnalogWorkflowBusinessDate);
-                        result.setTimestamp(endAnalogWorkflowBusinessDate);
-                    } else {
-                        log.error("SEARCH LAST SEND_ANALOG_FEEDBACK DETAILS NULL element {}", result);
-                        throw new PnInternalException("SEND_ANALOG_FEEDBACK NOT PRESENT, ERROR IN MAPPING", PnTimelineServiceExceptionCodes.ERROR_CODE_TIMELINESERVICE_TIMELINE_ELEMENT_NOT_PRESENT);
-                    }
-                }
-                case COMPLETELY_UNREACHABLE_CREATION_REQUEST, COMPLETELY_UNREACHABLE -> {
-                    //Se per lo stesso destinatario è presente un elemento di ANALOG_FAILURE_WORKFLOW, viene presa come riferimento la data di questo evento.
-                    Instant analogFailureWorkflowDate = getAnalogFailureWorkflowDate((RecipientRelatedTimelineElementDetails) result.getDetails(), timelineElementInternalSet, result.getIun());
-                    if (analogFailureWorkflowDate != null)
-                        result.setTimestamp(analogFailureWorkflowDate);
-                    else
-                        throw new PnInternalException("ANALOG_FAILURE_WORKFLOW NOT PRESENT, ERROR IN MAPPING", PnTimelineServiceExceptionCodes.ERROR_CODE_TIMELINESERVICE_TIMELINE_ELEMENT_NOT_PRESENT);
-                }
+                case SCHEDULE_REFINEMENT -> caseScheduleRefinement(timelineElementInternalSet, result);
+                case ANALOG_SUCCESS_WORKFLOW, ANALOG_WORKFLOW_RECIPIENT_DECEASED -> caseAnalogWorkflow(timelineElementInternalSet, result);
+                case COMPLETELY_UNREACHABLE_CREATION_REQUEST, COMPLETELY_UNREACHABLE -> caseUnreachable(timelineElementInternalSet, result);
                 case REFINEMENT -> //Viene recuperato per lo stesso destinatario un elemento di tipologia SCHEDULE_REFINEMENT e
                                    //dal dettaglio di quest’ultimo viene estrapolata la data che aveva registrato per l’esecuzione futura del processo di perfezionamento.
                         caseRefinement(timelineElementInternalSet, result);
@@ -68,6 +38,42 @@ public class TimelineMapperAfterFix extends TimelineMapper {
             //In ultima istanza viene settato l'eventTimestamp con il timestamp rimappato (avranno dunque in uscita sempre lo stesso valore)
             result.setEventTimestamp(result.getTimestamp());
         }
+    }
+
+    private void caseAnalogWorkflow(Set<TimelineElementInternal> timelineElementInternalSet, TimelineElementInternal result) {
+        //Viene presa come riferimento la data dell’evento più recente tra PREPARE_ANALOG_DOMICILE_FAILURE e SEND_ANALOG_FEEDBACK
+        Instant endAnalogWorkflowBusinessDate = computeEndAnalogWorkflowBusinessData((RecipientRelatedTimelineElementDetails) result.getDetails(), timelineElementInternalSet, result.getIun());
+        if (endAnalogWorkflowBusinessDate != null) {
+            log.debug("MAP TIMESTAMP: elem category {}, elem previous timestamp {}, elem new timestamp {} ", result.getCategory(), result.getTimestamp(), endAnalogWorkflowBusinessDate);
+            result.setTimestamp(endAnalogWorkflowBusinessDate);
+        } else {
+            log.error("SEARCH LAST SEND_ANALOG_FEEDBACK DETAILS NULL element {}", result);
+            throw new PnInternalException("SEND_ANALOG_FEEDBACK NOT PRESENT, ERROR IN MAPPING", PnTimelineServiceExceptionCodes.ERROR_CODE_TIMELINESERVICE_TIMELINE_ELEMENT_NOT_PRESENT);
+        }
+    }
+
+    private void caseScheduleRefinement(Set<TimelineElementInternal> timelineElementInternalSet, TimelineElementInternal result) {
+        //Se per lo stesso destinatario è presente un elemento di ANALOG_FAILURE_WORKFLOW, viene presa come riferimento la data di questo evento.
+        //In caso contrario, viene presa come riferimento la data dell’evento più recente tra PREPARE_ANALOG_DOMICILE_FAILURE e SEND_ANALOG_FEEDBACK
+        Instant analogFailureWorkflowDate = getAnalogFailureWorkflowDate((RecipientRelatedTimelineElementDetails) result.getDetails(), timelineElementInternalSet, result.getIun());
+        if (analogFailureWorkflowDate != null) {
+            result.setTimestamp(analogFailureWorkflowDate);
+        } else {
+            Instant endAnalogWorkflowBusinessDate = computeEndAnalogWorkflowBusinessData((RecipientRelatedTimelineElementDetails) result.getDetails(), timelineElementInternalSet, result.getIun());
+            if (endAnalogWorkflowBusinessDate != null) {
+                log.debug("MAP TIMESTAMP: elem category {}, elem previous timestamp {}, elem new timestamp {} ", result.getCategory(), result.getTimestamp(), endAnalogWorkflowBusinessDate);
+                result.setTimestamp(endAnalogWorkflowBusinessDate);
+            }
+        }
+    }
+
+    private void caseUnreachable(Set<TimelineElementInternal> timelineElementInternalSet, TimelineElementInternal result) {
+        //Se per lo stesso destinatario è presente un elemento di ANALOG_FAILURE_WORKFLOW, viene presa come riferimento la data di questo evento.
+        Instant analogFailureWorkflowDate = getAnalogFailureWorkflowDate((RecipientRelatedTimelineElementDetails) result.getDetails(), timelineElementInternalSet, result.getIun());
+        if (analogFailureWorkflowDate != null)
+            result.setTimestamp(analogFailureWorkflowDate);
+        else
+            throw new PnInternalException("ANALOG_FAILURE_WORKFLOW NOT PRESENT, ERROR IN MAPPING", PnTimelineServiceExceptionCodes.ERROR_CODE_TIMELINESERVICE_TIMELINE_ELEMENT_NOT_PRESENT);
     }
 
     private Instant getAnalogFailureWorkflowDate(RecipientRelatedTimelineElementDetails elementDetails, Set<TimelineElementInternal> timelineElementInternalSet, String iun) {
