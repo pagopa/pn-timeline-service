c0ce4e9f6eefe4df6f2c89ddcf0fefe88710e7f2
empassaro <emanuele.passaro@emeal.nttdata.com>
2026-05-21 18:05:36 +0200

    [PN-19956] chore: added log to trace resolve communication type with default

--- src/main/java/it/pagopa/pn/timelineservice/operations/CommunicationTypeClassifier.java
+++ src/main/java/it/pagopa/pn/timelineservice/operations/CommunicationTypeClassifier.java
@@ -3,6 +3,7 @@ package it.pagopa.pn.timelineservice.operations;
 import it.pagopa.pn.commons.exceptions.PnInternalException;
 import it.pagopa.pn.timelineservice.dto.timeline.CommunicationType;
 import it.pagopa.pn.timelineservice.dto.timeline.TimelineElementInternal;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
@@ -11,11 +12,13 @@ import java.util.Objects;
 import static it.pagopa.pn.timelineservice.exceptions.PnTimelineServiceExceptionCodes.ERROR_CODE_TIMELINESERVICE_UNKNOWN_COMMUNICATION_TYPE;
 
 @Component
+@Slf4j
 public class CommunicationTypeClassifier {
     private static final CommunicationType DEFAULT_TYPE = CommunicationType.LEGAL;
 
     public CommunicationType resolveFromTimelineElements(Collection<TimelineElementInternal> elements) {
         if (elements == null || elements.isEmpty()) {
+            log.debug("Timeline elements collection is null or empty, returning default communication type: {}", DEFAULT_TYPE);
             return DEFAULT_TYPE;
         }
         return elements.stream()
