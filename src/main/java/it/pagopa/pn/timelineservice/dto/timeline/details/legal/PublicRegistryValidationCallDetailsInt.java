0e666a6cd0850112565771a31518e92c9b3f9e04
CapolongoDa <daniele.capolongo@emeal.nttdata.com>
2026-06-24 16:18:30 +0200

    [PN-20304] feat: moved class from "common" package

--- src/main/java/it/pagopa/pn/timelineservice/dto/timeline/details/legal/PublicRegistryValidationCallDetailsInt.java
+++ src/main/java/it/pagopa/pn/timelineservice/dto/timeline/details/legal/PublicRegistryValidationCallDetailsInt.java
@@ -1,6 +1,7 @@
-package it.pagopa.pn.timelineservice.dto.timeline.details.common;
+package it.pagopa.pn.timelineservice.dto.timeline.details.legal;
 
-import it.pagopa.pn.timelineservice.dto.timeline.details.legal.DeliveryModeInt;
+import it.pagopa.pn.timelineservice.dto.timeline.details.common.CategoryTypeTimelineElementDetailsInt;
+import it.pagopa.pn.timelineservice.dto.timeline.details.common.TimelineElementDetailsInt;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
--- src/main/java/it/pagopa/pn/timelineservice/dto/timeline/details/legal/ValidateNormalizeAddressDetailsInt.java
+++ src/main/java/it/pagopa/pn/timelineservice/dto/timeline/details/legal/ValidateNormalizeAddressDetailsInt.java
@@ -1,5 +1,7 @@
-package it.pagopa.pn.timelineservice.dto.timeline.details.common;
+package it.pagopa.pn.timelineservice.dto.timeline.details.legal;
 
+import it.pagopa.pn.timelineservice.dto.timeline.details.common.CategoryTypeTimelineElementDetailsInt;
+import it.pagopa.pn.timelineservice.dto.timeline.details.common.TimelineElementDetailsInt;
 import it.pagopa.pn.timelineservice.utils.AuditLogUtils;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
